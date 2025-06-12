04eeb4f35f8473ac5743ac8e41c819c71b7046c9
“Gaia <st90070279279@gmail.com>
2025-06-12 19:28:51 +0800

    簡化抽屜元素

--- cube-web/src/components/layout/Basic/Drawer.tsx
+++ cube-web/src/components/layout/Basic/Drawer.tsx
@@ -3,17 +3,11 @@ import React, { useId } from "react";
 export interface DrawerProps {
   id?: string;
   renderSide?: React.ReactNode | (() => React.ReactNode);
-  renderButton?:
-    | React.ReactNode
-    | ((callBack: {
-        labelProps: Pick<HTMLLabelElement, "htmlFor" | "className">;
-      }) => React.ReactNode);
 }
 
 export default function Drawer({
   id,
   renderSide,
-  renderButton,
   children,
 }: React.PropsWithChildren<DrawerProps>) {
   const _id = useId();
@@ -22,18 +16,11 @@ export default function Drawer({
   return (
     <div className="drawer drawer-end flex-1">
       <input id={drawerId} type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col">
-        {children}
-        {typeof renderButton === "function"
-          ? renderButton({
-              labelProps: { htmlFor: drawerId, className: "drawer-button" },
-            })
-          : renderButton}
-      </div>
+      <div className="drawer-content flex flex-col">{children}</div>
       <div className="drawer-side z-20">
         <label
           htmlFor={drawerId}
-          aria-label="close sidebar"
+          aria-label="close drawer"
           className="drawer-overlay"
         />
         {typeof renderSide === "function" ? renderSide() : renderSide}
