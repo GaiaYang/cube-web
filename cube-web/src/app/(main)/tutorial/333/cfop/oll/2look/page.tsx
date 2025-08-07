dc69e295ad03ca237c3e70eaa5e59e6ff8783d07
“Gaia <st90070279279@gmail.com>
2025-08-08 01:29:31 +0800

    調整菜單

---
+++ cube-web/src/app/(main)/tutorial/333/cfop/oll/2look/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { type Metadata } from "next";
+
+import Article from "@/components/Article";
+
+export const metadata: Metadata = {
+  title: "兩段式OLL",
+  description: "將OLL分成兩個階段復原，大幅簡化需要記憶的公式。",
+};
+
+export default function Page() {
+  return (
+    <main>
+      <Article>
+        <h1>兩段式OLL</h1>
+        <p>將OLL分成兩個階段復原，大幅簡化需要記憶的公式。</p>
+      </Article>
+    </main>
+  );
+}
---
+++ cube-web/src/app/(main)/tutorial/333/cfop/pll/2look/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { type Metadata } from "next";
+
+import Article from "@/components/Article";
+
+export const metadata: Metadata = {
+  title: "兩段式PLL",
+  description: "將PLL分成兩個階段復原，大幅簡化需要記憶的公式。",
+};
+
+export default function Page() {
+  return (
+    <main>
+      <Article>
+        <h1>兩段式PLL</h1>
+        <p>將PLL分成兩個階段復原，大幅簡化需要記憶的公式。</p>
+      </Article>
+    </main>
+  );
+}
