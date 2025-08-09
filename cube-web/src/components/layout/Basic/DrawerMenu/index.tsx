ebdf7165cc6826caf58adfa340681700edc5bf61
“Gaia <st90070279279@gmail.com>
2025-08-09 13:30:17 +0800

    使用nanoId生成菜單id

--- cube-web/src/components/layout/Basic/DrawerMenu/index.tsx
+++ cube-web/src/components/layout/Basic/DrawerMenu/index.tsx
@@ -11,42 +11,49 @@ export default function DrawerMenu() {
   return <ul className="menu w-full">{options.map(_renderNode)}</ul>;
 }
 
-function MenuNode(item: MenuOption) {
+function MenuNode({
+  title,
+  href,
+  submenu,
+  collapsible,
+  asTitle,
+  // id,
+}: MenuOption) {
   // menu-title（純文字）
-  if (item.asTitle && !item.submenu) {
-    return <li className="menu-title">{item.title}</li>;
+  if (asTitle && !submenu) {
+    return <li className="menu-title">{title}</li>;
   }
 
   // menu-title + 子菜單
-  if (item.asTitle && item.submenu) {
+  if (asTitle && submenu) {
     return (
       <li>
-        <h2 className="menu-title">{item.title}</h2>
-        <ul>{item.submenu.map(_renderNode)}</ul>
+        <h2 className="menu-title">{title}</h2>
+        <ul>{submenu.map(_renderNode)}</ul>
       </li>
     );
   }
 
   // 可折疊父層
-  if (item.collapsible && item.submenu) {
+  if (collapsible && submenu) {
     return (
       <li>
         <details open={false}>
           <summary>
-            <MenuLink href={item.href}>{item.title}</MenuLink>
+            <MenuLink href={href}>{title}</MenuLink>
           </summary>
-          <ul>{item.submenu.map(_renderNode)}</ul>
+          <ul>{submenu.map(_renderNode)}</ul>
         </details>
       </li>
     );
   }
 
   // 一般有子菜單的父層（可點擊）
-  if (item.submenu) {
+  if (submenu) {
     return (
       <li>
-        <MenuLink href={item.href}>{item.title}</MenuLink>
-        <ul>{item.submenu.map(_renderNode)}</ul>
+        <MenuLink href={href}>{title}</MenuLink>
+        <ul>{submenu.map(_renderNode)}</ul>
       </li>
     );
   }
@@ -54,11 +61,11 @@ function MenuNode(item: MenuOption) {
   // 單純連結或文字
   return (
     <li>
-      <MenuLink href={item.href}>{item.title}</MenuLink>
+      <MenuLink href={href}>{title}</MenuLink>
     </li>
   );
 }
 
-function _renderNode(item: MenuOption, index: number) {
-  return <MenuNode {...item} key={index} />;
+function _renderNode(item: MenuOption) {
+  return <MenuNode {...item} key={item.id} />;
 }
