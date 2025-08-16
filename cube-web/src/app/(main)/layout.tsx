98c2d0057741e3b6e605b70b9a194819ee87e4c8
“Gaia <st90070279279@gmail.com>
2025-08-17 00:39:40 +0800

    調整版型

--- cube-web/src/app/(main)/layout.tsx
+++ cube-web/src/app/(main)/layout.tsx
@@ -5,7 +5,7 @@ import BasicLayout from "@/components/layout/Basic";
 export default function layout({ children }: React.PropsWithChildren) {
   return (
     <BasicLayout>
-      <main className="py-6">{children}</main>
+      <main className="pt-6 pb-24">{children}</main>
     </BasicLayout>
   );
 }
--- cube-web/src/app/(main)/tutorial/333/page.tsx
+++ cube-web/src/app/(main)/tutorial/333/page.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import { type Metadata } from "next";
 
 import Article from "@/components/Article";
+import DrawerMenuOverview from "@/components/DrawerMenuOverview";
 
 export const metadata: Metadata = {
-  title: "方塊教學總覽",
+  title: "魔術方塊教學總覽",
   description: "這裡列出作者本人的學習精華，讓想要速解的玩家有明確方向學習。",
   alternates: { canonical: "/tutorial/333" },
 };
@@ -12,8 +13,9 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <Article>
-      <h1>方塊教學總覽</h1>
+      <h1>魔術方塊教學總覽</h1>
       <p>這裡列出作者本人的學習精華，讓想要速解的玩家有明確方向學習。</p>
+      <DrawerMenuOverview />
     </Article>
   );
 }
