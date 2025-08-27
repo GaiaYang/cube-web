8c4e641f85a86d7da6cb2020be6e05cf12977bb3
“Gaia <st90070279279@gmail.com>
2025-08-27 16:26:09 +0800

    主題按鈕調整寫法來製作簡單動畫

--- cube-web/src/components/layout/Basic/ThemeButton.tsx
+++ cube-web/src/components/layout/Basic/ThemeButton.tsx
@@ -1,52 +1,64 @@
 "use client";
 
 import React, { useCallback } from "react";
-import { MoonIcon, SunIcon, SunMoonIcon } from "lucide-react";
+import { LucideProps, MoonIcon, SunIcon, SunMoonIcon } from "lucide-react";
 import { useTheme } from "next-themes";
 
+import cn from "@/utils/cn";
 import useMounted from "@/hooks/useMounted";
 
-export default function ThemeButton() {
+/**
+ * 切換網站主題模式的按鈕
+ * - `system`: 跟隨瀏覽器 / 作業系統
+ * - `light`: 強制亮色
+ * - `dark`: 強制暗色
+ */
+export default function ThemeToggleButton() {
   const mounted = useMounted();
   const { theme, setTheme, themes } = useTheme();
 
-  // 循環切換 (system → light → dark → system ...)
-  const toggleTheme = useCallback(() => {
-    setTheme((value) => {
-      const index = themes.indexOf(value);
+  /** 循環切換 theme */
+  const handleToggleTheme = useCallback(() => {
+    setTheme((current) => {
+      const index = themes.indexOf(current);
       const nextIndex = (index + 1) % themes.length;
       return themes[nextIndex];
     });
   }, [setTheme, themes]);
 
-  // 按鈕顯示模式
-  function _renderIcon() {
+  function renderIcons() {
     if (!mounted) {
       return <span className="loading loading-spinner" aria-hidden />;
     }
 
-    switch (theme) {
-      case "dark":
-        return <MoonIcon />;
-      case "light":
-        return <SunIcon />;
-      case "system":
-        return <SunMoonIcon />;
-      default:
-        return null;
-    }
+    // 使用 DaisyUI swap class 控制切換動畫
+    return themeIcons.map(({ id, Icon }) => (
+      <Icon key={id} className={cn(theme === id ? "swap-on" : "swap-off")} />
+    ));
   }
 
   return (
     <button
       type="button"
-      title="切換網站配色模式"
-      onClick={toggleTheme}
+      onClick={handleToggleTheme}
       disabled={!mounted}
-      className="btn btn-square btn-ghost"
+      title="切換網站配色模式"
+      className={cn("btn btn-square btn-ghost swap swap-active swap-rotate")}
     >
       <span className="sr-only">切換網站配色模式</span>
-      {_renderIcon()}
+      {renderIcons()}
     </button>
   );
 }
+
+/** 所有主題對應的 icon */
+const themeIcons: {
+  id: string;
+  Icon: React.ForwardRefExoticComponent<
+    Omit<LucideProps, "ref"> & React.RefAttributes<SVGSVGElement>
+  >;
+}[] = [
+  { id: "system", Icon: SunMoonIcon },
+  { id: "light", Icon: SunIcon },
+  { id: "dark", Icon: MoonIcon },
+];
