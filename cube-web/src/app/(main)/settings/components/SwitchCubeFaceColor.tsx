411e788606604e69e42d227dc0d1c67cfebff494
“Gaia <st90070279279@gmail.com>
2025-09-10 18:38:14 +0800

    顏色調整改用radio

--- cube-web/src/app/(main)/settings/components/SwitchCubeFaceColor.tsx
+++ cube-web/src/app/(main)/settings/components/SwitchCubeFaceColor.tsx
@@ -8,7 +8,7 @@ import { CubeFaceColors } from "@/enums/cube/color";
 
 import cn from "@/utils/cn";
 import getOppositeColor from "@/utils/cube/getOppositeColor";
-import { options, labels } from "@/options/cube/color";
+import { options } from "@/options/cube/color";
 import { getBgColor } from "@/themes/cube/colors";
 import { cubeFaceColorAtom, store, initialValue } from "@/jotai/settings";
 import useMounted from "@/hooks/useMounted";
@@ -27,70 +27,74 @@ export default function SwitchCubeFaceColor() {
   const frontOptions = notNilOptions.filter(
     (item) => !(item.value === cubeFaceColor.top || item.value === bottomColor),
   );
+  const isDisabled = !mounted;
 
   return (
     <div>
       <fieldset className="fieldset">
         <legend className="fieldset-legend">方塊頂面顏色調整</legend>
-        <div className="join">
-          {topOptions.map(({ id, value, label }) => (
-            <button
-              key={id}
-              type="button"
-              disabled={!mounted}
-              onClick={() => {
-                setCubeFaceColor((draft) => {
-                  draft.top = value;
-                  // 頂面替換預設替換前面顏色
-                  draft.front = topOptions.filter(
-                    (item) =>
-                      !(
-                        item.value === value ||
-                        item.value === getOppositeColor(value)
-                      ),
-                  )[0].value;
-                });
-              }}
-              className={cn("btn join-item", {
-                "btn-primary": cubeFaceColor.top === value,
-              })}
-            >
-              <ColorPreviewBox color={value} />
-              {label}
-            </button>
-          ))}
+        <div className="flex flex-wrap gap-6">
+          {topOptions.map(({ id, value, label }) => {
+            const isChecked = cubeFaceColor.top === value;
+            return (
+              <label key={id} className="flex items-center gap-2">
+                <input
+                  type="radio"
+                  name="cubeTopColor"
+                  disabled={isDisabled}
+                  checked={isChecked}
+                  onChange={(event) => {
+                    const checked = event.target.checked;
+                    if (checked) {
+                      setCubeFaceColor((draft) => {
+                        draft.top = value;
+                        // 頂面替換預設替換前面顏色
+                        draft.front = topOptions.filter(
+                          (item) =>
+                            !(
+                              item.value === value ||
+                              item.value === getOppositeColor(value)
+                            ),
+                        )[0].value;
+                      });
+                    }
+                  }}
+                  className="radio"
+                />
+                <ColorPreviewBox color={value} />
+                {label}
+              </label>
+            );
+          })}
         </div>
       </fieldset>
-      <div className="my-3 flex items-center gap-2">
-        <ColorPreviewBox
-          color={
-            options.find(({ value }) => value === bottomColor)?.value ??
-            CubeFaceColors.NONE
-          }
-        />
-        <p className="text-sm">{`當前底色為${labels[bottomColor ?? "none"]}`}</p>
-      </div>
       <fieldset className="fieldset">
         <legend className="fieldset-legend">方塊前面顏色調整</legend>
-        <div className="join">
-          {frontOptions.map(({ id, value, label }) => (
-            <button
-              key={id}
-              type="button"
-              disabled={!mounted}
-              onClick={() => {
-                setCubeFaceColor((draft) => {
-                  draft.front = value;
-                });
-              }}
-              className={cn("btn join-item", {
-                "btn-primary": cubeFaceColor.front === value,
-              })}
-            >
-              <ColorPreviewBox color={value} />
-              {label}
-            </button>
-          ))}
+        <div className="flex flex-wrap gap-6">
+          {frontOptions.map(({ id, value, label }) => {
+            const isChecked = cubeFaceColor.front === value;
+            return (
+              <label key={id} className="flex items-center gap-2">
+                <input
+                  type="radio"
+                  name="cubeFrontColor"
+                  checked={isChecked}
+                  disabled={isDisabled}
+                  onChange={(event) => {
+                    const checked = event.target.checked;
+                    if (checked) {
+                      setCubeFaceColor((draft) => {
+                        draft.front = value;
+                      });
+                    }
+                  }}
+                  className="radio"
+                />
+                <ColorPreviewBox color={value} />
+                {label}
+              </label>
+            );
+          })}
         </div>
       </fieldset>
     </div>
@@ -100,6 +104,7 @@ export default function SwitchCubeFaceColor() {
 function ColorPreviewBox({ color }: { color: CubeFaceColors }) {
   return (
     <span
+      aria-hidden="true"
       className={cn(
         "border-neutral/25 size-6 rounded border",
         getBgColor(color),
