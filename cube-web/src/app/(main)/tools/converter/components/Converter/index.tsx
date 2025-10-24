acbd4b60ea4882612d298b59493c8e46fda8bda9
“Gaia <st90070279279@gmail.com>
2025-10-25 03:13:13 +0800

    調整啟動專案為預設（加速器）、調整轉換器頁面組建為Activity

--- cube-web/src/app/(main)/tools/converter/components/Converter/index.tsx
+++ cube-web/src/app/(main)/tools/converter/components/Converter/index.tsx
@@ -3,15 +3,16 @@
 import React, { useMemo } from "react";
 import { Provider, useAtomValue } from "jotai";
 import dynamic from "next/dynamic";
+import { Activity } from "react";
 
 import type { CommonFormProps } from "./types";
-
 import { conversionFormLayoutAtom, conversionTabIndexAtom } from "./jotai";
 
 import FormModeToggle from "./FormModeToggle";
 import Tabs from "./Tabs";
 import CodeSpan from "../CodeSpan";
 import { ConverterPropsContext } from "./context";
+
 const StandForm = dynamic(() => import("./StandForm"));
 const InPlaceForm = dynamic(() => import("./InPlaceForm"));
 
@@ -32,51 +33,44 @@ export default function Converter() {
 function SwitchContent() {
   const tabIndex = useAtomValue(conversionTabIndexAtom);
 
-  switch (tabIndex) {
-    case 1:
-      return (
-        <>
-          <div>
-            <h3>三階額外符號</h3>
-            <ul className="[&>li>span]:flex [&>li>span]:gap-2">
-              <li>
-                <span>中間層</span>
-                <CodeSpan codes={["M", "S", "E"]} />
-              </li>
-              <li>
-                <span>非標準多層</span>
-                <CodeSpan codes={["r", "l", "u", "d", "f", "b"]} />
-              </li>
-            </ul>
-            <p>該區塊的轉換器額外支援三階非官方符號及特殊功能轉換</p>
-          </div>
+  return (
+    <>
+      <Activity mode={tabIndex === 0 ? "visible" : "hidden"}>
+        <FormEntry cubeOrder="nnn" />
+      </Activity>
+      <Activity mode={tabIndex === 1 ? "visible" : "hidden"}>
+        <div>
+          <h3>三階額外符號</h3>
+          <ul className="[&>li>span]:flex [&>li>span]:gap-2">
+            <li>
+              <span>中間層</span>
+              <CodeSpan codes={["M", "S", "E"]} />
+            </li>
+            <li>
+              <span>非標準多層</span>
+              <CodeSpan codes={["r", "l", "u", "d", "f", "b"]} />
+            </li>
+          </ul>
+          <p>該區塊的轉換器額外支援三階非官方符號及特殊功能轉換</p>
           <FormEntry cubeOrder="333" />
-        </>
-      );
-    case 0:
-    default:
-      return <FormEntry cubeOrder="nnn" />;
-  }
+        </div>
+      </Activity>
+    </>
+  );
 }
 
 function FormEntry({ cubeOrder }: CommonFormProps) {
   const formType = useAtomValue(conversionFormLayoutAtom);
   const value = useMemo(() => ({ cubeOrder }), [cubeOrder]);
 
-  function _render() {
-    switch (formType) {
-      case "stand":
-        return <StandForm />;
-      case "in-place":
-        return <InPlaceForm />;
-      default:
-        return null;
-    }
-  }
-
   return (
     <ConverterPropsContext.Provider value={value}>
-      {_render()}
+      <Activity mode={formType === "stand" ? "visible" : "hidden"}>
+        <StandForm />
+      </Activity>
+      <Activity mode={formType === "in-place" ? "visible" : "hidden"}>
+        <InPlaceForm />
+      </Activity>
     </ConverterPropsContext.Provider>
   );
 }
